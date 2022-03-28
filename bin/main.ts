8d774c283d08bd27e005c00f1e3f626099642ec6
schottsfired <david.schott@snyk.io>
2022-03-28 10:56:53 -0400

    revert 0bb804 because env vars are needed for ci

--- bin/main.ts
+++ bin/main.ts
@@ -4,18 +4,48 @@ import { SnykMonitorAddOn } from '../dist';
 
 const app = new App();
 
-// AWS Properties
-const account = '<account id>' // e.g. 492635582501
-const region = '<region>' // e.g. us-east-1
-const stackID = '<stack id>' // e.g. ssp-amazon-eks-snyk
+// load required parameters from the environment and validate them
+const account = process.env.ACCOUNT; // e.g. 492635582501
+const region = process.env.REGION || 'us-east-1';
+const stackID = process.env.STACK_ID || 'ssp-amazon-eks-snyk';
+const integrationId = process.env.INTEGRATION_ID; // e.g. abcd1234-abcd-1234-abcd-1234abcd1234
+if (!inputsAreValid()) {
+    console.log("Inputs are invalid. Exiting...");
+    process.exit(1);
+}
 const stackProps = { env: { account, region } }
 
-// Snyk Properties
-const integrationId = '<integration ID>' // e.g. abcd1234-abcd-1234-abcd-1234abcd1234
-
+// deploy EKS with the Snyk Monitor addon
 EksBlueprint.builder()
     .addOns(new SnykMonitorAddOn({
         integrationId: integrationId,
-        values: {} // additional Helm chart values
+        version: "1.86.0",
+        values: {}
     }))
     .build(app, stackID, stackProps);
+
+function inputsAreValid(): boolean {
+    let valid = true;
+    if (!account || account.length == 0) {
+        console.log("ACCOUNT environment variable is empty or unset.");
+        valid = false;
+    }
+    if (!process.env.REGION) {
+        console.log("REGION environment variable is unset. Will default to 'us-east-1'.");
+    }
+    if (!process.env.STACK_ID) {
+        console.log("STACK_ID environment variable is unset. Will default to 'ssp-amazon-eks-snyk'.");
+    }
+    if (!integrationId || integrationId.length == 0) {
+        console.log("INTEGRATION_ID environment variable is empty or unset.");
+        valid = false;
+    } else {
+        try {
+            atob(integrationId);
+        } catch (e) {
+            console.log("INTEGRATION_ID is not in base64 format.");
+            valid = false;
+        }
+    }
+    return valid;
+}
